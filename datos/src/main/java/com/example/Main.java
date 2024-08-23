09b8118af961fa7dff6eec20b4ed4903b8bb569f
Eimer96x <eimermenco1996@gmail.com>
2024-08-23 16:30:09 -0500

    estatura

--- datos/src/main/java/com/example/Main.java
+++ datos/src/main/java/com/example/Main.java
@@ -4,4 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
+    public class volar {
+    
+        String nombre;
+        int edad;
+        int estatura;
+
+        
+    }
 }
